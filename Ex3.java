5dea088294e6ecfca795f14e15999e43b89e550a
Hoàng Minh Đức <hoangminhduc30106@gmail.com>
2025-08-24 18:48:47 +0700

    Create Ex3.java

---
+++ Ex3.java
@@ -0,0 +1,19 @@
+package Lec5_Loop;
+import java.util.Scanner;
+public class Ex1_ {
+    public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in);
+      System.out.print("Enter height: ");
+        int height = sc.nextInt();
+        System.out.print("Enter width: ");
+        int width = sc.nextInt();
+        for (int i = 1; i <= height; i++) {
+            for (int j = 1; j <= width; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        System.out.println("\n");
+    }
+}
