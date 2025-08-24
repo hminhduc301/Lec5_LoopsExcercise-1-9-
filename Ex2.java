e3b277675ce16317b9a5127030739a7d4b4ff08f
Hoàng Minh Đức <hoangminhduc30106@gmail.com>
2025-08-24 18:46:31 +0700

    Create Ex2.java

---
+++ Ex2.java
@@ -0,0 +1,17 @@
+package Lec5_Loop;
+import java.util.Scanner;
+public class Ex1_ {
+    public static void main(String[] args) {
+      System.out.print("Enter an integer from 2 to 9: ");
+        int n = sc.nextInt();
+        if (n >= 2 && n <= 9) {
+            for (int i = 1; i <= 10; i++) {
+                System.out.println(n + " x " + i + " = " + (n * i));
+            }
+        } else {
+            System.out.println("Invalid input! Please enter between 2 and 9.");
+        }
+
+        System.out.println("\n");
+    }
+}
