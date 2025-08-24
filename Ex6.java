2d4715610308430dd6f65c45add1368646d7a535
Hoàng Minh Đức <hoangminhduc30106@gmail.com>
2025-08-24 19:20:00 +0700

    Create Ex6.java

---
+++ Ex6.java
@@ -0,0 +1,22 @@
+package Lec5_Loop;
+import java.util.Scanner;
+import java.util.Random;
+public class Ex6 {
+    public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in);
+       Random random = new Random();
+        int secret = random.nextInt(100) + 1;
+        int guess;
+        do {
+            System.out.print("Enter your guess (1-100): ");
+            guess = sc.nextInt();
+            if (guess > secret) {
+                System.out.println("Your guess is higher than the secret number!");
+            } else if (guess < secret) {
+                System.out.println("Your guess is lower than the secret number!");
+            } else {
+                System.out.println("Congratulations! You guessed correctly!");
+            }
+        } while (guess != secret);
+    }
+}
