a0a331f2a219268ff29a6fb7206102556318fff4
Hoàng Minh Đức <hoangminhduc30106@gmail.com>
2025-08-24 17:25:07 +0700

    Create Ex1.java

---
+++ Ex1.java
@@ -0,0 +1,15 @@
+package Lec5_Loop;
+import java.util.Scanner;
+public class Ex1_ {
+    public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in);
+      System.out.print("Enter a positive integer N: ");
+        int N = sc.nextInt();
+        int sum = 0;
+        for (int i = 1; i <= N; i++) {
+            sum += i;
+        }
+        System.out.println("The sum of numbers from 1 to " + N + " is: " + sum);
+        System.out.println("\n");
+    }
+}
