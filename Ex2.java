24102fc95e2b5614bbc86e7df743e5b744f44a59
Hoàng Minh Đức <hoangminhduc30106@gmail.com>
2025-08-24 18:53:13 +0700

    Update Ex2.java

--- Ex2.java
+++ Ex2.java
@@ -1,6 +1,6 @@
 package Lec5_Loop;
 import java.util.Scanner;
-public class Ex1_ {
+public class Ex2 {
     public static void main(String[] args) {
       Scanner sc = new Scanner(System.in);
       System.out.print("Enter an integer from 2 to 9: ");
