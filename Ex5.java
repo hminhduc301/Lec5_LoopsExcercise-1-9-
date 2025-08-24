b0a717e2a22a06fe9e7272be4b7d0986271b4eff
Hoàng Minh Đức <hoangminhduc30106@gmail.com>
2025-08-24 18:52:50 +0700

    Create Ex5.java

---
+++ Ex5.java
@@ -0,0 +1,16 @@
+package Lec5_Loop;
+import java.util.Scanner;
+public class Ex1_ {
+    public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in);
+      System.out.print("Enter a non-negative integer: ");
+        int f = sc.nextInt();
+        long factorial = 1;
+        for (int i = f; i >= 1; i--) {
+            factorial *= i;
+        }
+        System.out.println("The factorial of " + f + " is: " + factorial);
+
+        System.out.println("\n");
+    }
+}
