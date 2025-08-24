fe90c6defe5987231b2e49a18d0e5ae77b31769e
Hoàng Minh Đức <hoangminhduc30106@gmail.com>
2025-08-24 19:02:11 +0700

    Update Ex5.java

--- Ex5.java
+++ Ex5.java
@@ -1,6 +1,6 @@
 package Lec5_Loop;
 import java.util.Scanner;
-public class Ex1_ {
+public class Ex5 {
     public static void main(String[] args) {
       Scanner sc = new Scanner(System.in);
       System.out.print("Enter a non-negative integer: ");
