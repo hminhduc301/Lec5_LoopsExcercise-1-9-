0215388319e5fd03e580fa086ace4c1731aed7a5
Hoàng Minh Đức <hoangminhduc30106@gmail.com>
2025-08-25 14:37:48 +0700

    Update Ex8.java

--- Ex8.java
+++ Ex8.java
@@ -1,1 +1,16 @@
-
+package Lec5_Loops;
+import java.util.Scanner;
+public class Ex8 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number between 1 and 10: ");
+        int n;
+        do{
+            n = scanner.nextInt();
+            if(n < 1 || n > 10){
+                System.out.print("Invalid number, please try again: ");
+            }
+        }while(n<1||n>10); 
+        System.out.println("You entered the number: "+n);
+    }
+}
